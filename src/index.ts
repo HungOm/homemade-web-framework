a6f583fba9d488e13b1acc5cec5a53abfcccfd5c
Hung Om <hung.workspace@gmail.com>
2022-11-17 00:27:21 +0800

    moved collection to User as static collection method

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@
 import { Collection } from "./models/Collections";
 import { User, UserProps } from "./models/User";
 
-const collection = new Collection<User,UserProps>("http://localhost:3000/users",(json: UserProps) => User.buildUser(json)); 
-
+const collection = User.buildUserCollection();
 collection.on("change", () => {
     const users = collection.models;
     console.log(users);
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from "./Model";
 import { Attributes } from "./Attributes";
 import { Eventing } from "./eventing";
 import { ApiSync } from "./ApiSync";
+import { Collection } from "./Collections";
 export interface UserProps {
     id?: number;
     name?:string;
@@ -11,6 +12,10 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
+    static buildUserCollection(): Collection<User,UserProps> {
+        return new Collection<User,UserProps>(rootUrl,(json: UserProps) => User.buildUser(json));
+    }
+
 
     static buildUser(attrs: UserProps): User {
         return new User(
